a18681f187e7191fdaceb5d45f5eb79dedee855f
ronnyr <ronnyr@wix.com>
2023-01-18 13:31:36 +0200

    use new sdk

--- packages/wix/src/fetcher.ts
+++ packages/wix/src/fetcher.ts
@@ -1,32 +1,23 @@
 import { API_URL, WIX_ACCESS_TOKEN_COOKIE, WIX_DOMAIN, WIX_REFRESH_TOKEN_COOKIE, WIX_COOKIE_EXPIRE } from './const'
-import { handleFetchResponse, getCustomerToken } from './utils'
+import { handleFetchResponse } from './utils'
 import Cookies from 'js-cookie'
+import { createClient, OAuthStrategy } from '@wix/sdk';
+import { products } from '@wix/stores'
+
 
 const fetcher: any = async ({
   method = 'POST',
   url,
   variables
 }: any) => {
+  const wixClient = createClient({modules: {products}, auth: OAuthStrategy({clientId: '6730773d-e547-4beb-ab89-6c480166c29d'})})
   let accessToken = Cookies.get(WIX_ACCESS_TOKEN_COOKIE)
   let refreshToken = Cookies.get(WIX_REFRESH_TOKEN_COOKIE)
-  let customerToken = getCustomerToken();
-  if (!accessToken) {
-    const res = await fetch(
-      `${API_URL}/v1/meta-site/session-token`,
-      {
-        method: 'POST',
-        headers: {
-          'origin': WIX_DOMAIN!,
-          'Content-Type': 'application/json',
-          ...(customerToken && {'member-token': customerToken}),
-          ...(refreshToken && {'refresh-token': refreshToken})
-        }
-      })
-    const json = await res.json()
-    accessToken = json.accessToken
-    Cookies.set(WIX_ACCESS_TOKEN_COOKIE, accessToken!, {expires: 0.3})
-    Cookies.set(WIX_REFRESH_TOKEN_COOKIE, refreshToken || json.refreshToken!, {expires: WIX_COOKIE_EXPIRE})
-  }
+  const wixSession = await wixClient.auth.generateVisitorTokens({accessToken, refreshToken});
+
+  accessToken = wixSession.accessToken;
+  Cookies.set(WIX_ACCESS_TOKEN_COOKIE, accessToken!, {expires: 0.3})
+  Cookies.set(WIX_REFRESH_TOKEN_COOKIE, refreshToken || wixSession.refreshToken!, {expires: WIX_COOKIE_EXPIRE})
 
   return handleFetchResponse(
     await fetch(url[0] === '/' ? url : `${API_URL}/${url}`, {
--- packages/wix/src/fetcherNew.ts
+++ packages/wix/src/fetcherNew.ts
@@ -1,20 +1,19 @@
-import { WIX_ACCESS_TOKEN_COOKIE, WIX_DOMAIN, WIX_REFRESH_TOKEN_COOKIE, WIX_COOKIE_EXPIRE } from './const'
+import { WIX_ACCESS_TOKEN_COOKIE, WIX_REFRESH_TOKEN_COOKIE, WIX_COOKIE_EXPIRE } from './const'
 import Cookies from 'js-cookie'
 import { cart, currentCart } from '@wix/ecom'
 import { products } from '@wix/stores'
-import { createClient, session } from '@wix/sdk';
+import { createClient, OAuthStrategy } from '@wix/sdk';
 
-const wixClient = createClient({cart, products, currentCart})
-
+const wixClient = createClient({modules: {cart, products, currentCart}, auth: OAuthStrategy({clientId: '6730773d-e547-4beb-ab89-6c480166c29d'})})
 export type clientTypes = typeof wixClient
 
-const fetcher = async (): Promise<typeof wixClient> => {
+const fetcher = async (): Promise<clientTypes> => {
   let accessToken = Cookies.get(WIX_ACCESS_TOKEN_COOKIE) ?? ''
   let refreshToken = Cookies.get(WIX_REFRESH_TOKEN_COOKIE) ?? ''
-  const wixSession = await session({ refreshToken, accessToken }, { domain: WIX_DOMAIN!} );
-  Cookies.set(WIX_ACCESS_TOKEN_COOKIE, wixSession.accessToken!, { expires: 0.3 })
+  const wixSession = await wixClient.auth.generateVisitorTokens({accessToken, refreshToken});
+  Cookies.set(WIX_ACCESS_TOKEN_COOKIE, wixSession.accessToken!, { expires: 0.1 })
   Cookies.set(WIX_REFRESH_TOKEN_COOKIE, wixSession.refreshToken!, { expires: WIX_COOKIE_EXPIRE })
-  wixClient.setSession(wixSession)
+  wixClient.auth.setTokens(wixSession)
 
   return wixClient
 }
